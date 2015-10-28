dc3a4138cd0cfa570b334bd4ea8445764346b1b3
cliffjones <cliffjones@gmail.com>
2015-10-27 22:56:03 -0500

    Add more usage instructions

--- polli.py
+++ polli.py
@@ -9,6 +9,13 @@ Once a large enough corpus of utterances has accumulated, it should be possible
 for such a system to generalize syntactic rules, but that is beyond the scope
 of this project.
 
+When you run the script, it will look for JSON data in the accompanying text
+files. If these are removed, the script will still function, but it will
+require patience as conversational skill is acquired sentence by sentence. What
+you put in is what you get back. To end a conversation, simply submit a blank
+response. Changes to the data files will only be saved if the program is ended
+in this way.
+
 This code is the original work of Cliff Jones (http://CliffJonesJr.com). It may
 be modified for personal use, but it should not be used commercially, and
 modified versions may not be distributed without accreditation and the author's
